67f8469d1ab97dffbd9d02509449c9d92f097050
anirbanpranto <anirbanpranto@gmail.com>
2023-10-26 22:57:27 +0800

    feat: modifications for extensibility and efficient client handling

--- raven/raven.py
+++ raven/raven.py
@@ -1,11 +1,10 @@
-from typing import Optional, Tuple
-from slack_sdk import WebClient
-from typing import Dict
 import os
 import logging
 import datetime
 import sys
 import traceback
+from typing import Optional, Dict
+from slack_sdk import WebClient
 
 logging.basicConfig(
     level=logging.INFO,
@@ -13,39 +12,33 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-
 class Raven:
-    def __init__(
-        self,
-        channels: Dict,
-        token: Optional[str] = None,
-        service: Optional[str] = "",
-        environment: str = "dev",
-        client_type="slack",
-        cloudwatch: Optional[str] = None,
-        custom_fields: Dict = {},
-    ) -> None:
-        """
-        `channels`: Dictionary containing `channel_name` and `channel_id` \n
-        `token`: Token for clients, if the value is not passed, we look for BOT_TOKEN variable in .env file \n
-        `service`: Service that the bot is running on \n
-        `environment`: Environment the service is running on, default is dev \n
-        `client_type`: Client to be used to send alerts. By default it is Slack, its the only available client right now.
-        """
-        self.cloudwatch = None
-        if cloudwatch and "HOSTNAME" in os.environ:
-            logger.info(f"Initializing alertbot at {Raven._get_pod_info()}")
-            self.cloudwatch = cloudwatch + Raven._get_pod_info()
-        self.token = token
-        self.channels = channels
-        self.client_type = client_type
-        self.client = Raven._get_client(self.token, self.client_type)
-        self.env = environment
-        self.service = service
-        self.custom_fields = custom_fields
+    def __init__(self) -> None:
+        # the purpose of the class is to encapsulate related implementations and caching clients
+        pass 
 
     @staticmethod
-    def send_error_logs(
+    def send_generic_log(
+        message: str,
+        channel_id: Optional[str] = None,
+        token: Optional[str] = None,
+        service: Optional[str] = None,
+        environment: Optional[str] = None,
+        client_type="slack",
+    ):
+        try:
+            mkdown = Raven.get_markdown(
+                message=message,
+                env=environment,
+                service=service,
+            )
+            client = Raven._get_client(token=token, client_type=client_type)
+            Raven._send_log(client, channel_id, mkdown)
+        except Exception as e:
+            logger.debug(e)
+
+    @staticmethod
+    def send_error_log(
         channel_id: Optional[str] = None,
         token: Optional[str] = None,
         service: Optional[str] = None,
@@ -64,13 +57,11 @@ class Raven:
                 custom_fields=custom_fields,
             )
             client = Raven._get_client(token=token, client_type=client_type)
-            err, _ = Raven._send_log(client, channel_id, mkdown)
-            if err:
-                logger.debug(err)
-                print(err)
+            Raven._send_error_log(client, channel_id, mkdown)
         except Exception as e:
             logger.debug(e)
-
+    
+    # client provider, this would always contain a map of functions that can be passed a token to initialize a client
     @staticmethod
     def _get_client_mappings(client_type):
         client_dict = {"slack": WebClient}
@@ -88,46 +79,51 @@ class Raven:
     def _get_client(token: Optional[str], client_type: str):
         if not token:
             raise Exception("Please check presence of your token")
+        Raven.client_type = client_type
         client = Raven._get_client_mappings(client_type=client_type)
-        return client(token=token)
+        if not Raven.client:
+            Raven.client = client(token=token)
+        return Raven.client
 
     @staticmethod
-    def get_alertbot_instance(
-        channels: Dict,
-        token: Optional[str] = None,
-        service: str = "",
-        environment: str = "dev",
-        client_type: str = "slack",
-        cloudwatch: Optional[str] = None,
-        custom_fields: Dict = {},
-    ):
-        bot_instance = Raven(
-            channels=channels,
-            service=service,
-            token=token,
-            environment=environment,
-            client_type=client_type,
-            cloudwatch=cloudwatch,
-            custom_fields=custom_fields,
-        )
-        return bot_instance
-
-    @staticmethod
-    def _send_log(client: WebClient, channel_id: Optional[str], msg: str) -> Tuple:
+    def _send_error_log(client: WebClient, channel_id: Optional[str], msg: str):
         if channel_id:
-            try:
-                res = client.chat_postMessage(
+            if Raven.client_type == "slack":
+                _ = client.chat_postMessage(
                     channel=channel_id,
                     text="Error Message",
                     blocks=[
                         {"type": "section", "text": {"type": "mrkdwn", "text": msg}}
                     ],
                 )
-                return None, res
-            except Exception as e:
-                return e, None
         else:
             raise Exception("Please enter a channel_id")
+    
+    @staticmethod
+    def _send_log(client: WebClient, channel_id: Optional[str], msg: str):
+        if channel_id:
+            client.chat_postMessage(
+                channel=channel_id,
+                text="Generaic Message",
+                blocks=[
+                    {"type": "section", "text": {"type": "mrkdwn", "text": msg}}
+                ],
+            )
+        else:
+            raise Exception("Please enter a channel_id")
+    
+
+    @staticmethod
+    def get_markdown(
+        message: str,
+        env: Optional[str],
+        service: Optional[str],
+    ):
+        t = datetime.datetime.utcnow() + datetime.timedelta(hours=8)
+        t = t.strftime("%m/%d/%Y, %H:%M:%S")
+        return (
+                f"*Time*: `{t}`\n*Environment*: `{env}`\n*Service*: `{service}`\n*Message*: ```Traceback: {message}\n```"
+        )
 
     @staticmethod
     def get_error_markdown(
@@ -140,8 +136,8 @@ class Raven:
         t = t.strftime("%m/%d/%Y, %H:%M:%S")
         type, value, _ = sys.exc_info()
         trace = str(traceback.format_exc())
-        if len(trace) >= 3000:
-            trace = trace[-2800:]
+        if len(trace) >= 2000:
+            trace = trace[-2000:]
         if not cloudwatch:
             return (
                 f"*Time*: `{t}`\n*Environment*: `{env}`\n*Service*: `{service}`\n*Stack Trace*: ```Type: {type}\nTraceback: {trace}\nError: {value}\n```"
@@ -161,32 +157,3 @@ class Raven:
                 )
             )
 
-    def send_generic_log(
-        self, channels: Dict, channel: Optional[str] = None, msg: str = ""
-    ) -> None:
-        try:
-            channel_id = (
-                channels[list(channels.keys())[0]] if not channel else channels[channel]
-            )
-            err, _ = self._send_log(self.client, channel_id, msg)
-            if err:
-                logger.debug(err)
-        except Exception as e:
-            logger.debug(e)
-
-    def send_error_log(self, channel_id: str) -> None:
-        """
-        `channel`: Channel name of the channel to send the alert to. \n
-        `error`: An exception raised by the application.
-        """
-        try:
-            mkdown = Raven.get_error_markdown(
-                self.env, self.service, self.cloudwatch, self.custom_fields
-            )
-            err, _ = Raven._send_log(self.client, channel_id, mkdown)
-            if err:
-                print(err)
-                logger.debug(err)
-        except Exception as e:
-            print(e)
-            logger.debug(e)
